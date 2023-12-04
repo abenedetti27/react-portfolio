da4cf1969a9818d011178766366029fa60e18c84
abenedetti27 <145300062+abenedetti27@users.noreply.github.com>
2023-12-03 21:02:56 -0800

    remaining component shells

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -8,3 +8,5 @@ const Portfolio = () => {
     </div>
   );
 };
+
+export default Portfolio;
---
+++ src/components/Resume.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Resume = () => {
+    return (
+        <div>
+            <h1>Resume</h1>
+            <h2>My Resume</h2>
+        </div>
+    );
+};
+
+export default Resume;
